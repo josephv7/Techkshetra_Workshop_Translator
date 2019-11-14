ee8a51d7f9c2b256cbc520e7c61c6221a34bd4d1
iamjosephvarghese <josephvarghese.rms@gmail.com>
2019-11-14 18:26:36 +0530

    :sparkles: Text translation working

--- app/src/main/java/com/iamjosephvarghese/languagetranslator/MainActivity.java
+++ app/src/main/java/com/iamjosephvarghese/languagetranslator/MainActivity.java
@@ -1,14 +1,120 @@
 package com.iamjosephvarghese.languagetranslator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText output, input;
+    FirebaseTranslator englishGermanTranslator;
+    Button button;
+    boolean downloaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        output = findViewById(R.id.output);
+        input = findViewById(R.id.input);
+        button = findViewById(R.id.button);
+
+        button.setVisibility(View.GONE);
+
+
+        FirebaseTranslatorOptions options =
+                new FirebaseTranslatorOptions.Builder()
+                        .setSourceLanguage(FirebaseTranslateLanguage.EN)
+                        .setTargetLanguage(FirebaseTranslateLanguage.DE)
+                        .build();
+        englishGermanTranslator =
+                FirebaseNaturalLanguage.getInstance().getTranslator(options);
+
+
+
+
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+                .build();
+        englishGermanTranslator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener(
+                        new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void v) {
+                                // Model downloaded successfully. Okay to start translating.
+                                // (Set a flag, unhide the translation UI, etc.)
+
+                                downloaded = true;
+                                button.setVisibility(View.VISIBLE);
+
+
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Model couldn’t be downloaded or other internal error.
+                                // ...
+                            }
+                        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                translateText();
+            }
+        });
+
+
+
+    }
+
+
+
+    public  void  translateText(){
+
+        if(input.getText().toString() != null){
+
+            englishGermanTranslator.translate(input.getText().toString())
+                    .addOnSuccessListener(
+                            new OnSuccessListener<String>() {
+                                @Override
+                                public void onSuccess(@NonNull String translatedText) {
+
+                                    Log.d("translatedtext",translatedText);
+                                    output.setText(translatedText);
+                                }
+                            })
+                    .addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Error.
+                                    // ...
+                                    Log.d("error","not translated");
+                                }
+                            });
+
+        }
+
+
+
+
+
     }
 }
